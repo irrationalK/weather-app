1e63ed906c527314f5befac3bad2cc5eac7d4502
kevin.lu <kevin.lu@live.de>
2023-10-04 12:50:34 +0200

    Add logic to fetch weather data

--- App.js
+++ App.js
@@ -2,31 +2,11 @@ import React, { useState, useEffect } from "react"
 import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import Tabs from "./src/components/Tabs"
-import * as Location from 'expo-location'
+import { useGetWeather } from "./hooks/useGetWeather"
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [location, setLocation] = useState(null)
-  const [error, setError] = useState(null)
-
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync
-      if (status !== 'granted') {
-        setError('permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLocation(location)
-    })()
-  }, [])
-
-  if (location) {
-    console.log(location)
-  } else {
-    console.log("no data")
-  }
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
 
   if (loading) {
     return (
